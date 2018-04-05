eaf2965dc9411a657d23c56fd0208df320292393
elliot richardson <ear78@aol.com>
2018-04-04 22:54:21 -0500

    continued onward toward completion

---
+++ wp-content/themes/wphierarchy/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_theme_support( 'title-tag' );
+add_theme_support('post-thumbnails');
+add_theme_support( 'post_format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
+add_theme_support('html5');
+add_theme_support( 'automatic_feed_links' );
+add_theme_support( 'custom-background' );
+add_theme_support( 'custom-header' );
+add_theme_support( 'custom-logo' );
+add_theme_support( 'customize-selective-refresh-widgets' );
+add_theme_support( 'starter-content' );
+
+// Load in our CSS
+function wphierarchy_enqueue_styles(){
+    wp_enqueue_style( '')
+}
+
+
+
+
+
+
+ ?>
