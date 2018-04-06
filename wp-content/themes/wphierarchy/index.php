916a72cc77db22e0ff054ef3bd8e6f3d2f08c4a5
elliot richardson <ear78@aol.com>
2018-04-05 23:45:47 -0500

    working on adding simple mark up for wordpress

--- wp-content/themes/wphierarchy/index.php
+++ wp-content/themes/wphierarchy/index.php
@@ -1,6 +1,24 @@
 
-<?php get_header( 'splash'); ?>
+<?php get_header(); ?>
 
-        <h1>index.php</h1>
-    </body>
-</html>
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
+
+            </main>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                <header class="entry-header">
+
+                </header>
+            </article>
+
+            <div>index.php</div>
+
+            <div class="entry-content">
+                <p>Lorem</p>
+            </div>
+
+        </div>
+
+<?php get_footer() ?>
